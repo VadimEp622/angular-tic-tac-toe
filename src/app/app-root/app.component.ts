89597f19517563b9463ab9e37422c0f305f0ff62
Vlad Epov <vladep622@gmail.com>
2024-03-24 05:31:43 +0200

    styled home page, and added TODO's

--- src/app/app-root/app.component.ts
+++ src/app/app-root/app.component.ts
@@ -11,4 +11,9 @@ import { AppHeaderComponent } from '../cmps/app-header/app-header.component';
 })
 export class AppComponent {
   title = 'angular-tic-tac-toe';
+
+  // TODO: add an app encompassing loader bar at the top of the page (line bar), for viewing that user action to change route is happening
+
+  // TODO (optional): make a game service, which has a list of game objects, by which we can make game routes, and game buttons in home page
+  // OR... think about making game/:id route, and consider how to handle the different game's services
 }
